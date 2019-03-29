67a9f62b95e8e1a2215647f665882f1752e23de2
HIRABOU <owner@ownernoMacBook-puro.local>
2019-03-29 11:16:04 +0900

    Edit html and sass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(current_user)
-    @tweets = user.tweets.order("created_at DESC")
-    @user = User.find(params[:id])
+    @user = User.find(current_user)
+    @tweets = @user.tweets.order("created_at DESC")
   end
 
 end
