b289f98796a32dd51ea289bf7418c73d85721788
HIRABOU <owner@ownernoMacBook-puro.local>
2019-03-29 09:33:29 +0900

    Add index reverse

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.page(params[:page]).per(8).order('created_at DESC')
   end
 
   # GET /tweets/1
